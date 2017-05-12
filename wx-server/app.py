340fc551412e297260c4dfaade39530217147c4d
xucan <xucan@huantengsmart.com>
2017-05-12 15:00:02 +0800

    set host and port

--- wx-server/app.py
+++ wx-server/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "hello, world, wx"
 
+@app.route("/test")
+def test():
+    return 'test'
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
