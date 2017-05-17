0cff8fd6b7c958c79ddaf6240a87b187a850b2db
xucan <xucan@huantengsmart.com>
2017-05-17 10:19:30 +0800

    add index.html

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 from app import app
-# app.run(debug=True)
-app.run('0.0.0.0', port=443, ssl_context=('/etc/letsencrypt/live/tongying.design/fullchain.pem', '/etc/letsencrypt/live/tongying.design/privkey.pem'))
+app.run('0.0.0.0', port=80)
+# app.run('0.0.0.0', port=443, ssl_context=('/etc/letsencrypt/live/tongying.design/fullchain.pem', '/etc/letsencrypt/live/tongying.design/privkey.pem'))
